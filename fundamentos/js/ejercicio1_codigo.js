98da619d6b7307f917c9343518d93771a4fbfc46
Patricio <pcifrey501@g.educaand.es>
2023-09-27 11:39:23 +0200

    primer commit

---
+++ fundamentos/js/ejercicio1_codigo.js
@@ -0,0 +1,6 @@
+console.log("Hola Mundo!\n"+"Soy el primer script");
+
+/*
+    Este mensaje me va a imprimir Hola mundo, luego con el \n hace un salto de línea y procede a imprimir
+    el segundo mensaje
+*/
