302c8933cffe38f2e2216950eb806ac2be3d8b1f
Patricio <pcifrey501@g.educaand.es>
2023-10-12 13:20:58 +0200

    ejercicio 07 terminado por fin

--- ACT02_Objetos/js/obj_codigo_ej07.js
+++ ACT02_Objetos/js/obj_codigo_ej07.js
@@ -5,11 +5,74 @@ segundo que pasa la pantalla debe de disminuir en tamaño de 40 en 40 pixeles ta
 ancho y cuando tenga una medida menor o igual a 160 x 160 empiece a crecer hasta un tamaño no mayor que 400x400.
 */
 
-const abrir_ventana= () =>{
-   clients.openWindow()
+let ancho=400
+let altura=400
+let ventana
+let temporizador_aumento
+let temporizador_disminucion
+
+
+const abrir_ventana= (ancho_estandar, altura_estandar, posicion_horizontal_inicial, posicion_vertical_inicial) =>{
+
+    ventana=window.open("", "prueba",
+        `resizable=yes,width=${ancho_estandar},height=${altura_estandar},left=${posicion_horizontal_inicial},top=${posicion_vertical_inicial}`)
+
+}
+const disminuir_tamanio=()=>{
+    ancho-=40
+    altura-=40
+    ventana.resizeBy(-40, -40)
+    console.log(ancho);
+    if(ancho===160){
+
+        //Al llegar a 160  limpio su intervalo de ejecución y creo uno para llamar a aumentar
+
+        clearInterval(temporizador_disminucion)
+        temporizador_aumento=setInterval(aumentar_tamanio, 1000)
+
+    }
+    cerrar_ventana(temporizador_disminucion)
 }
 
+const aumentar_tamanio=()=>{
+    ancho+=40
+    altura+=40
+    ventana.resizeBy(40, 40)
+    console.log(ancho);
+    if(ancho===400){
+
+        //limpio el intervalo y creo el de aumentar, de esta forma siempre al llegar a los límites s
+        // e llama a la función contraria y se ejecuta
+
+        clearInterval(temporizador_aumento)
+        temporizador_disminucion=setInterval(disminuir_tamanio, 1000)
+    }
+    cerrar_ventana(temporizador_aumento)
+}
+
+
+//función para limpiar los intervalos al cerrar la ventana nueva
+const cerrar_ventana= (temporizador)=>{
+    if (ventana.closed){
+        console.log("cerrada")
+        clearInterval(temporizador)
+    }
+}
+
+
 document.getElementById("pulsa").addEventListener("click", () =>{
-    abrir_ventana
-    alert("funciona")
+    ancho=400
+    altura=400
+    let posicion_horizontal=Math.round(Math.random()*(window.screen.width-1+1)+1)
+    let posicion_vertical=Math.round(Math.random()*(window.screen.height-1+1)+1)
+    abrir_ventana(ancho, altura, posicion_horizontal, posicion_vertical)
+    temporizador_disminucion=setInterval(disminuir_tamanio, 1000)
+
 })
+
+
+///eeeeeeeeeeeee
+
+
+
+
---
+++ ACT02_Objetos/js/obj_codigo_ej08.js
@@ -0,0 +1,14 @@
+/*
+Diseña una página web que:
+Compruebe si existen las cookies de los datos de una persona.
+Si no existen, que pida los datos personales:
+Nombre
+Dirección
+Edad
+Profesión
+Si existen los datos que los muestre y posteriormente borre las cookies.
+Realiza las mismas operaciones anteriores con el objeto localStorage, en este caso los datos permanecerán almacenados
+mientras dure la sesión, cuando se vuelva a entrar, los datos se habrán borrado.
+En la misma sesión mostrará los datos cada vez que se pidan.
+ */
+
